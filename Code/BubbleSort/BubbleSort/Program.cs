75857e05943e5ed75feed599f0b6d5de2e7a7986
amaargiru <war4one@gmail.com>
2022-02-07 17:06:51 +0500

    Add Bubble Sort

---
+++ Code/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+int[] a = { 800, 11, 50, 771, 649, 770, 240, 9 };
+
+for (int i = 1; i <= a.Length - 1; ++i)
+{
+   for (int j = 0; j < a.Length - i; ++j)
+   {
+      if (a[j] > a[j + 1])
+      {
+         Swap(ref a[j], ref a[j + 1]);
+      }
+   }
+}
+
+[MethodImpl(MethodImplOptions.AggressiveInlining)]
+static void Swap(ref int x, ref int y)
+{
+   var temp = x;
+   x = y;
+   y = temp;
+}
